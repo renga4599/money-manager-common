5d9b30f13c6fb1c61c332c457a66515b34a67ec8
Rengarajan Kannan <renga4599@Rengarajans-MacBook-Pro.local>
2021-12-21 18:51:01 -0600

    first commit

---
+++ src/main/java/renga/money/manager/MoneyManagerCommonApplication.java
@@ -0,0 +1,13 @@
+package renga.money.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagerCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyManagerCommonApplication.class, args);
+	}
+
+}
---
+++ src/main/java/renga/money/manager/models/common/UserDetails.java
@@ -0,0 +1,9 @@
+package renga.money.manager.models.common;
+
+import lombok.Data;
+
+@Data
+public class UserDetails {
+    private String name;
+    private int age;
+}
